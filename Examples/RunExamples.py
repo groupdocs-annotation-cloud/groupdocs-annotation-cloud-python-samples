5fb6c88d0e173198753d8483ea0a75606479fa41
Sergei Terentev <sergei.terentev@aspose.com>
2021-06-18 09:52:38 +0300

    Samples update for use with SDK v21.6

--- Examples/RunExamples.py
+++ Examples/RunExamples.py
@@ -25,26 +25,26 @@ GetDocumentInfo.Run()
 from AdvancedUsage.AddAnnotations.AddAreaAnnotation import AddAreaAnnotation
 AddAreaAnnotation.Run()
 
-# from AdvancedUsage.AddAnnotations.AddArrowAnnotation import AddArrowAnnotation
-# AddArrowAnnotation.Run()
+from AdvancedUsage.AddAnnotations.AddArrowAnnotation import AddArrowAnnotation
+AddArrowAnnotation.Run()
 from AdvancedUsage.AddAnnotations.AddMultipleAnnotations import AddMultipleAnnotations
 AddMultipleAnnotations.Run()
-# from AdvancedUsage.AddAnnotations.AddPointAnnotation import AddPointAnnotation
-# AddPointAnnotation.Run()
-# from AdvancedUsage.AddAnnotations.AddPolylineAnnotation import AddPolylineAnnotation
-# AddPolylineAnnotation.Run()
-# from AdvancedUsage.AddAnnotations.AddTextFieldAnnotation import AddTextFieldAnnotation
-# AddTextFieldAnnotation.Run()
-# from AdvancedUsage.AddAnnotations.AddTextRedactionAnnotation import AddTextRedactionAnnotation
-# AddTextRedactionAnnotation.Run()
-# from AdvancedUsage.AddAnnotations.AddTextReplacementAnnotation import AddTextReplacementAnnotation
-# AddTextReplacementAnnotation.Run()
-# from AdvancedUsage.AddAnnotations.AddTextStrikeoutAnnotation import AddTextStrikeoutAnnotation
-# AddTextStrikeoutAnnotation.Run()
-# from AdvancedUsage.AddAnnotations.AddTextUnderlineAnnotation import AddTextUnderlineAnnotation
-# AddTextUnderlineAnnotation.Run()
-# from AdvancedUsage.AddAnnotations.AddWatermarkAnnotation import AddWatermarkAnnotation
-# AddWatermarkAnnotation.Run()
+from AdvancedUsage.AddAnnotations.AddPointAnnotation import AddPointAnnotation
+AddPointAnnotation.Run()
+from AdvancedUsage.AddAnnotations.AddPolylineAnnotation import AddPolylineAnnotation
+AddPolylineAnnotation.Run()
+from AdvancedUsage.AddAnnotations.AddTextFieldAnnotation import AddTextFieldAnnotation
+AddTextFieldAnnotation.Run()
+from AdvancedUsage.AddAnnotations.AddTextRedactionAnnotation import AddTextRedactionAnnotation
+AddTextRedactionAnnotation.Run()
+from AdvancedUsage.AddAnnotations.AddTextReplacementAnnotation import AddTextReplacementAnnotation
+AddTextReplacementAnnotation.Run()
+from AdvancedUsage.AddAnnotations.AddTextStrikeoutAnnotation import AddTextStrikeoutAnnotation
+AddTextStrikeoutAnnotation.Run()
+from AdvancedUsage.AddAnnotations.AddTextUnderlineAnnotation import AddTextUnderlineAnnotation
+AddTextUnderlineAnnotation.Run()
+from AdvancedUsage.AddAnnotations.AddWatermarkAnnotation import AddWatermarkAnnotation
+AddWatermarkAnnotation.Run()
 
 from AdvancedUsage.ExtractAnnotations.ExtractAnnotations import ExtractAnnotations
 ExtractAnnotations.Run()
